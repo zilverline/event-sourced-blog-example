44aa2cf08d51fd77a16b8b3b8aaea9cf396438b9
Erik Rozendaal <erik@deler.org>
2012-06-24 17:12:50 +0200

    Simplified memory image access.

--- app/controllers/PostsController.scala
+++ app/controllers/PostsController.scala
@@ -7,14 +7,14 @@ import play.api.mvc._
 import play.api.data._
 import play.api.data.Forms._
 import play.api.data.validation.Constraints._
-import views.html.defaultpages.{ notFound, todo }
+import views.html.defaultpages.notFound
 import events._
 import models._
 import support.Mappings._
 
 object PostsController extends Controller {
 
-  val memoryImage = {
+  val posts = {
     val initialEvents = Seq(
       PostCreated(UUID.fromString("4e885ffe-870e-45b4-b5dd-f16d381d6f6a"), PostContent("Erik", "Scala is awesome", "Scala...")),
       PostCreated(UUID.fromString("4e885ffe-870e-45b4-b5dd-f16d381d6f6f"), PostContent("Bas", "Righteous Ruby", "Ruby...")))
@@ -22,8 +22,7 @@ object PostsController extends Controller {
     Ref(initialValue).single
   }
 
-  def posts = memoryImage()
-  def commit(event: PostEvent) = memoryImage.transform(_.apply(event))
+  def commit(event: PostEvent) = posts.transform(_.apply(event))
 
   val postContentForm = Form(mapping(
     "author" -> trimmedText.verifying(minLength(3)),
@@ -31,11 +30,11 @@ object PostsController extends Controller {
     "content" -> trimmedText.verifying(minLength(3)))(PostContent.apply)(PostContent.unapply))
 
   def index = Action { implicit request =>
-    Ok(views.html.posts.index(posts.mostRecent(20)))
+    Ok(views.html.posts.index(posts().mostRecent(20)))
   }
 
   def show(id: UUID) = Action { implicit request =>
-    posts.get(id) match {
+    posts().get(id) match {
       case Some(post) => Ok(views.html.posts.show(post))
       case None       => NotFound(notFound(request, None))
     }
@@ -50,12 +49,12 @@ object PostsController extends Controller {
       formWithErrors => BadRequest(views.html.posts.create(id, formWithErrors)),
       postContent => {
         commit(PostCreated(id, postContent))
-        Redirect(routes.PostsController.index).flashing("info" -> "Post created.")
+        Redirect(routes.PostsController.show(id)).flashing("info" -> "Post created.")
       })
   }
 
   def renderEdit(id: UUID) = Action { implicit request =>
-    posts.get(id) match {
+    posts().get(id) match {
       case Some(post) => Ok(views.html.posts.edit(id, postContentForm.fill(post.content)))
       case None       => NotFound(notFound(request, None))
     }
@@ -66,7 +65,7 @@ object PostsController extends Controller {
       formWithErrors => BadRequest(views.html.posts.edit(id, formWithErrors)),
       postContent => {
         commit(PostUpdated(id, postContent))
-        Redirect(routes.PostsController.index).flashing("info" -> "Post saved.")
+        Redirect(routes.PostsController.show(id)).flashing("info" -> "Post saved.")
       })
   }
 
