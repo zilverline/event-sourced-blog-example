103dab048b9b263f95ed51a4a98742c94626088b
Erik Rozendaal <erik@deler.org>
2012-06-24 11:12:34 +0200

    Apply changes to memory image in same transaction as event store commit.

--- app/eventstore/fake.scala
+++ app/eventstore/fake.scala
@@ -9,22 +9,26 @@ class FakeEventStore[A](onCommit: Commit[A] => Unit) extends EventStore[A] {
   private[this] val stored = TMap.empty[UUID, Seq[CommittedEvent[A]]]
 
   override def commit[B <: A, R](streamId: UUID, expectedVersion: Int, events: B*)(completed: Either[Conflict[A], Commit[B]] => R): R = {
-    val commit = atomic { implicit txn =>
+    val conflictOrCommit = atomic { implicit txn =>
       val existingEvents = stored.get(streamId).getOrElse(Vector.empty)
       val actualVersion = existingEvents.size
       if (expectedVersion != actualVersion) {
         Left(Conflict(actualVersion, existingEvents.drop(expectedVersion)))
       } else {
         val committedAt = DateTimeUtils.currentTimeMillis
-        val committedEvents = events.zipWithIndex.map { case (event, index) => CommittedEvent(streamId, existingEvents.size + index + 1, committedAt, event) }
+        val committedEvents = events.zipWithIndex.map { case (event, index) => CommittedEvent(streamId, actualVersion + index + 1, committedAt, event) }
+        stored.put(streamId, existingEvents ++ committedEvents)
+
         val commitSequence = commitCount()
         commitCount += 1
-        stored.put(streamId, existingEvents ++ committedEvents)
-        Right(Commit(commitSequence, committedAt, committedEvents))
+        val commit = Commit(commitSequence, committedAt, committedEvents)
+
+        onCommit(commit)
+
+        Right(commit)
       }
     }
 
-    commit.right.foreach(onCommit)
-    completed(commit)
+    completed(conflictOrCommit)
   }
 }
