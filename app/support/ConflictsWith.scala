f625c9de17a4aec8b14e7b7e814e98ff5e34e298
Erik Rozendaal <erik@deler.org>
2012-08-13 09:32:16 +0200

    Refactoring.

--- app/support/ConflictsWith.scala
+++ app/support/ConflictsWith.scala
@@ -9,14 +9,14 @@ trait ConflictsWith[-Event] {
    * Checks each `committed` event for conflicts against all `attempted` events.
    * Any committed event that conflicts is returned.
    */
-  def conflicting[Committed <: Event, Attempted <: Event](committed: Seq[Committed], attempted: Seq[Attempted]): Seq[Committed]
+  def conflicting[A <: Event, B <: Event](committed: Seq[A], attempted: Seq[B]): Seq[A]
 }
 object ConflictsWith {
   /**
    * Builds a new `ConflictsWith` based on the given predicate.
    */
   def apply[Event](predicate: (Event, Event) => Boolean) = new ConflictsWith[Event] {
-    override def conflicting[Committed <: Event, Attempted <: Event](committed: Seq[Committed], attempted: Seq[Attempted]): Seq[Committed] =
+    override def conflicting[A <: Event, B <: Event](committed: Seq[A], attempted: Seq[B]): Seq[A] =
       committed.filter(a => attempted.exists(b => predicate(a, b)))
   }
 }
